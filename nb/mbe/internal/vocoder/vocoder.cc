f252a95256e2b1bf7989f2511c65e55591b5b2ba
Mark Butcher <mark.butcher@trellisdata.com.au>
2025-05-26 14:25:02 +1000

    reduce logging

--- nb/mbe/internal/vocoder/vocoder.cc
+++ nb/mbe/internal/vocoder/vocoder.cc
@@ -226,7 +226,7 @@ void vocoder_imbe_encode(vocoder coder, int16_t *frame_vector, int16_t *snd)
 
 void vocoder_imbe_decode(vocoder coder, int16_t *snd, int16_t *frame_vector)
 {
-		std::cout << "decode" << snd << std::endl;
+		// std::cout << "decode" << snd << std::endl;
     coder->imbe_decode(frame_vector, snd);
 }
 
